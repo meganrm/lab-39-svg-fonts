42e635d11b1b6f6da01925372cb18837c83aff53
meganr <meganrm@gmail.com>
2018-01-27 14:21:58 -0800

    added a spinning rocket

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
 
 import FileData from '../FileData';
 import Auth from '../Auth';
@@ -7,7 +8,15 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <header> Visual Files
+        <header>
+
+        Visual Files
+          <FontAwesome
+            name="rocket"
+            size="2x"
+            spin
+            style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+          />
         </header>
         <main>
           <Auth>
